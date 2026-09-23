0df63a0ffcbcb9bbc08ccafe840a70e988fa43e0
dheerajsolanki613-arch <dheerajsolanki613@gmail.com>
2026-09-23 19:03:08 +0530

    Fix StudySpace Timer checkpoint 11

--- app/src/main/java/com/studyspace/timer/screens/tasks/TaskEditorDialog.kt
+++ app/src/main/java/com/studyspace/timer/screens/tasks/TaskEditorDialog.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.FilterChip
